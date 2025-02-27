19bcfd7b7bb77267a954008d6813567e803094cc
Noah Amsel <noah.amsel@nyu.edu>
2025-02-27 11:11:18 -0500

    baseline

--- run_directly.sh
+++ run_directly.sh
@@ -1,6 +1,6 @@
 conda activate MPP
 
-run_name="demo"
+run_name="demo2"
 config="basic_config"   # options are "basic_config" for all or swe_only/comp_only/incomp_only/swe_and_incomp
 yaml_config="./config/mpp_avit_ti_config.yaml"
 
--- submit_batch.sh
+++ submit_batch.sh
@@ -1,41 +1,24 @@
 #!/bin/bash -l
-#SBATCH --time=4:00:00
-#SBATCH -p gpu
+
+#SBATCH --job-name=demoohoh
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --gpus-per-node=8
 #SBATCH --cpus-per-gpu=8
-#SBATCH -J demo
-#SBATCH --open-mode=append
-#SBATCH -C h100
-
+#SBATCH --mem=48G
+#SBATCH --time=00-04:00:00       # Run time (dd-hh:mm:ss)
+#SBATCH --output=/scratch/nia4240/mpp-scratch/slurm/%x/%j.out
+#SBATCH --gres=gpu:1
+#SBATCH --constraint=a100
 
 export HDF5_USE_FILE_LOCKING=FALSE
 export OMP_NUM_THREADS=6
 
-master_node=$SLURMD_NODENAME
-VENVDIR=/venvs/
-run_name="demo"
-config="basic_config"   # options are "basic_config" for all or swe_only/comp_only/incomp_only/swe_and_incomp
-yaml_config="./config/mpp_avit_ti_config.yaml"
+export run_name=$SLURM_JOB_NAME/$SLURM_JOB_ID
+export config="basic_config"   # options are "basic_config" for all or swe_only/comp_only/incomp_only/swe_and_incomp
+export yaml_config="./config/mpp_avit_ti_config.yaml"
 
-
-module purge
-module load modules/2.2-20230808
-module load gcc openmpi/4.0.7 python-mpi/3.11.2
-
-module load hdf5/mpi-1.12.2
-module load cuda/12.1 cudnn/cuda12-8.9.0 nccl/cuda12.1-2.18.1 
-
-# conda activate pdebench
-source $VENVDIR/pdebench_venv/bin/activate
-set -x
-
-srun python `which torchrun` \
-	--nnodes=$SLURM_JOB_NUM_NODES \
- 	--nproc_per_node=$SLURM_GPUS_PER_NODE \
-	--rdzv_id=$SLURM_JOB_ID \
-		--rdzv_backend=c10d \
-		--rdzv_endpoint=$master_node:29500 \
-		train_basic.py --run_name $run_name --config $config --yaml_config $yaml_config --use_ddp
-
+singularity exec --nv --overlay /scratch/nia4240/overlay-50G-10M.ext3:ro /scratch/work/public/singularity/cuda12.1.1-cudnn8.9.0-devel-ubuntu22.04.2.sif /bin/bash -c "
+source /ext3/env.sh
+conda activate MPP
+python train_basic.py --run_name $run_name --config $config --yaml_config $yaml_config 2>&1
+"
