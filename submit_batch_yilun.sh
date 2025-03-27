b4b7273447e09d037f91604bb69b52a7cbab56eb
YilunKuang <yilun.kuang@nyu.edu>
2025-03-26 20:33:35 -0400

    add muP and change config

--- submit_batch_yilun.sh
+++ submit_batch_yilun.sh
@@ -16,10 +16,12 @@ export OMP_NUM_THREADS=6
 export run_name=$SLURM_JOB_NAME/$SLURM_JOB_ID
 export config="basic_config"   # options are "basic_config" for all or swe_only/comp_only/incomp_only/swe_and_incomp
 export yaml_config="./config/mpp_avit_ti_config.yaml"
-export token_mixing_struct="bilinearbtt"
+# export token_mixing_struct="bilinearbtt"
+export token_mixing_struct=$1
+export learning_rate=$2
 
 singularity exec --nv --overlay /scratch/yk2516/singularity/noah/overlay-50G-10M.ext3:ro /scratch/work/public/singularity/cuda12.1.1-cudnn8.9.0-devel-ubuntu22.04.2.sif /bin/bash -c "
 source /ext3/env.sh
 conda activate MPP
-python train_basic.py --run_name $run_name --config $config --yaml_config $yaml_config 2>&1 --token_mixing_struct $token_mixing_struct
+python train_basic.py --run_name $run_name --config $config --yaml_config $yaml_config 2>&1 --token_mixing_struct $token_mixing_struct --learning_rate $learning_rate
 "
